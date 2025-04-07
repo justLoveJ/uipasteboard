1f9ce39037288d535967b4d800d1d9a606c334e6
Liplum <Li_plum@outlook.com>
2025-04-07 09:22:34 +0800

    Refactor: Add UIColor extension to convert color to hex string format

--- ios/uipasteboard/Sources/uipasteboard/UipasteboardPlugin.swift
+++ ios/uipasteboard/Sources/uipasteboard/UipasteboardPlugin.swift
@@ -173,3 +173,18 @@ public class UiPasteboardPlugin: NSObject, FlutterPlugin {
     }
   }
 }
+
+extension UIColor {
+  func toHexString() -> String {
+    var r: CGFloat = 0
+    var g: CGFloat = 0
+    var b: CGFloat = 0
+    var a: CGFloat = 0
+
+    getRed(&r, green: &g, blue: &b, alpha: &a)
+
+    let rgb: Int = (Int)(r * 255) << 24 | (Int)(g * 255) << 16 | (Int)(b * 255) << 8 | (Int)(a * 255) << 0
+
+    return String(format: "%08x", rgb).uppercased()
+  }
+}
